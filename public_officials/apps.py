81d8c540b19db499499e8b1bde8e1d991300c94b
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-10 14:53:11 -0600

    Initial commit

---
+++ public_officials/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PublicOfficialsConfig(AppConfig):
+    name = 'public_officials'
---
+++ public_officials/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.http import HttpRequest, HttpResponse
+from django.core.urlresolvers import resolve
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'public-officials/home.html')
