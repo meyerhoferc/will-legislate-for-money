37bbfc91ca99212632d4fe9e791b123ffd166a7a
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-13 12:56:17 -0600

    Add VCR for requests

--- will_legislate_for_money/functional_tests/test_public_officials.py
+++ will_legislate_for_money/functional_tests/test_public_officials.py
@@ -3,6 +3,7 @@ import time
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import vcr
 
 class GuestUserTest(LiveServerTestCase):
 
@@ -13,11 +14,12 @@ class GuestUserTest(LiveServerTestCase):
         self.browser.quit()
 
     def test_check_for_correct_content_on_root(self):
-        self.browser.get(self.live_server_url)
-        self.assertIn('Will Legislate For Money', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Your One Stop Corruption Watch', header_text)
-        politician_text = self.browser.find_element_by_id('politicians').text
-        self.assertIn("Diana DeGette", politician_text)
-        self.assertIn("Jared Polis", politician_text)
-        self.assertIn("Scott Tipton", politician_text)
+        with vcr.use_cassette('cassettes/get_legislators'):
+            self.browser.get(self.live_server_url)
+            self.assertIn('Will Legislate For Money', self.browser.title)
+            header_text = self.browser.find_element_by_tag_name('h1').text
+            self.assertIn('Your One Stop Corruption Watch', header_text)
+            politician_text = self.browser.find_element_by_id('politicians').text
+            self.assertIn("Diana DeGette", politician_text)
+            self.assertIn("Jared Polis", politician_text)
+            self.assertIn("Scott Tipton", politician_text)
