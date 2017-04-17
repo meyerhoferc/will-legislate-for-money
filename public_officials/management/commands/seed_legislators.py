70f56de4af5c56281060bdbf768e6d92f44cc8a7
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-17 16:21:04 -0600

    Successfully seeded database with API call

--- public_officials/management/commands/seed_legislators.py
+++ public_officials/management/commands/seed_legislators.py
@@ -2,12 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 from public_officials.models import *
 from public_officials.services import *
 import csv
+import pdb
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Legislator.objects.all().delete()
         legislator_service = LegislatorService()
-        legislators = legislator_service.get_all_legislators
+        legislators = legislator_service.get_all_legislators()
         for legislator in legislators:
             Legislator.objects.create(
             first_name=legislator["first_name"],
@@ -23,4 +24,4 @@ class Command(BaseCommand):
             term_end=legislator["term_start"],
             party=legislator["party"]
             )
-            self.stdout.write(self.style.SUCCESS('Successfully created legislator %s' % row[0]))
+            self.stdout.write(self.style.SUCCESS('Successfully created legislator %s' % legislator["last_name"]))
