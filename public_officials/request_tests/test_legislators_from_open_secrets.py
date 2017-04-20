2f5c92189811a45303ba75515edf7fb405426492
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-19 21:54:39 -0600

    Add request test for voting history

--- public_officials/request_tests/test_legislators_from_open_secrets.py
+++ public_officials/request_tests/test_legislators_from_open_secrets.py
@@ -37,9 +37,20 @@ class LegislatorContributionDataTest(TestCase):
             self.assertEqual("Luther", legislators[0]["first_name"])
 
 class LegislatorSponsoredPolicyDataTest(TestCase):
+
     def test_service_gets_sponsored_bills_for_one_legislator(self):
         with vcr.use_cassette('cassettes/get_sponsored_bills'):
             legislator_service = LegislatorService()
             legislator_recent_bills = legislator_service.get_recent_bills('L000287')
             self.assertTrue(legislator_recent_bills)
             self.assertEqual("Missed Opportunities Act of 2017", legislator_recent_bills[0]["title"])
+
+class LegislatorVotingHistoryDataTest(TestCase):
+    
+    def test_service_gets_voting_history_for_one_legislator(self):
+        legislator_service = LegislatorService()
+        voting_history = legislator_service.get_voting_history('L000287')
+        self.assertTrue(voting_history)
+        self.assertEqual("Supporting America's Innovators Act of 2017", voting_history[0]["title"])
+        self.assertEqual("Yes", voting_history[0]["position"])
+        self.assertEqual("Passed", voting_history[0]["result"])
