729f9ce92c900867371e1b2ca32adb70f95fdc32
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-29 12:57:31 -0700

    Add recently updated bills template + styling

--- public_officials/templatetags/format_legislator_json.py
+++ public_officials/templatetags/format_legislator_json.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaulttags import register
+from public_officials.models import *
 import pdb
 
 register = template.Library()
@@ -13,3 +14,18 @@ def get_value(dictionary, key):
 def get_nested_value(dictionary, key):
     if key and type(dictionary) is dict:
         return dictionary['@attributes'][key]
+
+@register.filter
+def get_sponsor_url(bill):
+    legislators = Legislator.objects.filter(pid=bill['sponsor']['bioguideid'])
+    if legislators.count() == 1:
+        legislator_id = legislators.first().id
+        legislator_url = "/legislators/%s" % legislator_id
+        return legislator_url
+
+@register.filter
+def get_sponsor_name(bill):
+    legislator_first_name = bill['sponsor']['firstname']
+    legislator_last_name = bill['sponsor']['lastname']
+    legislator_full_name = legislator_first_name + " " + legislator_last_name
+    return legislator_full_name
