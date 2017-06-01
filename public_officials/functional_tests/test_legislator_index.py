01876a2c9ea25ed31e803eb2c4b10ca454e4c9b5
Katy Farmer <dreadpiratekaty@gmail.com>
2017-06-01 14:17:41 -0600

    Splits tests into separate, descriptive files

---
+++ public_officials/functional_tests/test_legislator_index.py
@@ -0,0 +1,119 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+from public_officials.models import *
+import os
+import vcr
+
+class GuestUserTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_checks_for_content_on_legislators_senators_index(self):
+        legislator_one = Legislator.objects.create(first_name="Diana",
+                                                   last_name="DeGette",
+                                                   phone="12345",
+                                                   email="email@email.com",
+                                                   state_name="Colorado",
+                                                   pid="D000197",
+                                                   chamber="house",
+                                                   term_start="2017-01-03",
+                                                   term_end="2019-01-03",
+                                                   party="D",
+                                                   state="CO",
+                                                   cid="N00006134")
+
+        legislator_two = Legislator.objects.create(first_name="Henry",
+                                                   last_name="Schmojo",
+                                                   phone="12345",
+                                                   email="email@email.com",
+                                                   state_name="Texas",
+                                                   pid="D000197",
+                                                   chamber="senate",
+                                                   term_start="2017-01-03",
+                                                   term_end="2019-01-03",
+                                                   party="D",
+                                                   state="TX",
+                                                   cid="N00006134")
+        legislator_three = Legislator.objects.create(first_name="Love",
+                                                     last_name="Fluff",
+                                                     phone="12345",
+                                                     email="email@email.com",
+                                                     state_name="Colorado",
+                                                     pid="D000197",
+                                                     chamber="senate",
+                                                     term_start="2017-01-03",
+                                                     term_end="2019-01-03",
+                                                     party="D",
+                                                     state="CO",
+                                                     cid="N00006134")
+
+        self.browser.get(self.live_server_url + '/senators/')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        texas_senator = self.browser.find_element_by_css_selector('#Texas-senators').text
+        colorado_senator = self.browser.find_element_by_css_selector('#Colorado-senators').text
+        self.assertIn("All Senators", header_text)
+        self.assertIn("Henry Schmojo", texas_senator)
+        self.assertNotIn("Henry Schmojo", colorado_senator)
+        self.assertIn("Love Fluff", colorado_senator)
+        self.assertNotIn("Love Fluff", texas_senator)
+        self.assertNotIn("Diana DeGette", texas_senator)
+        self.assertNotIn("Diana DeGette", colorado_senator)
+
+    def test_checks_for_content_on_legislators_representative_index(self):
+        legislator_one = Legislator.objects.create(first_name="Diana",
+                                                   last_name="DeGette",
+                                                   phone="12345",
+                                                   email="email@email.com",
+                                                   state_name="Colorado",
+                                                   pid="D000197",
+                                                   chamber="house",
+                                                   term_start="2017-01-03",
+                                                   term_end="2019-01-03",
+                                                   party="D",
+                                                   state="CO",
+                                                   cid="N00006134")
+
+        legislator_two = Legislator.objects.create(first_name="Henry",
+                                                   last_name="Schmojo",
+                                                   phone="12345",
+                                                   email="email@email.com",
+                                                   state_name="Texas",
+                                                   pid="D000197",
+                                                   chamber="house",
+                                                   term_start="2017-01-03",
+                                                   term_end="2019-01-03",
+                                                   party="D",
+                                                   state="TX",
+                                                   cid="N00006134")
+
+        legislator_three = Legislator.objects.create(first_name="Love",
+                                                     last_name="Fluff",
+                                                     phone="12345",
+                                                     email="email@email.com",
+                                                     state_name="Colorado",
+                                                     pid="D000197",
+                                                     chamber="senate",
+                                                     term_start="2017-01-03",
+                                                     term_end="2019-01-03",
+                                                     party="D",
+                                                     state="CO",
+                                                     cid="N00006134")
+
+        self.browser.get(self.live_server_url + '/representatives/')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        colorado_rep = self.browser.find_element_by_css_selector('#Colorado-reps').text
+        texas_rep = self.browser.find_element_by_css_selector('#Texas-reps').text
+        self.assertIn("All Representatives", header_text)
+        self.assertIn("Henry Schmojo", texas_rep)
+        self.assertNotIn("Henry Schmojo", colorado_rep)
+        self.assertIn("Diana DeGette", colorado_rep)
+        self.assertNotIn("Diana DeGette", texas_rep)
---
+++ public_officials/unit_tests/test_pages.py
@@ -0,0 +1,99 @@
+from django.test import TestCase
+from django.http import HttpRequest, HttpResponse
+from django.core.urlresolvers import resolve
+from public_officials.views import *
+from public_officials.models import *
+from django.contrib.auth.models import User
+import vcr
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'public-officials/home.html')
+
+class StatePageTest(TestCase):
+    def test_state_url_resolves_to_state_view(self):
+        found = resolve('/state/')
+        self.assertEqual(found.func, state_legislators)
+
+    def test_state_legislators_returns_correct_template(self):
+        response = self.client.get('/state/?state=Texas')
+        self.assertTemplateUsed(response, 'public-officials/state.html')
+
+class DetailPageTest(TestCase):
+    def test_detail_url_resolves_to_legislator_detail_view(self):
+        found = resolve('/legislators/1/')
+        self.assertEqual(found.func, legislator_detail)
+
+    def test_legislator_detail_page_returns_correct_template(self):
+        with vcr.use_cassette('cassettes/get_complete_legislator_profile'):
+            legislator = Legislator.objects.create(first_name="Diana",
+                                                   last_name="Degette",
+                                                   phone="12345",
+                                                   email="email@email.com",
+                                                   state_name="Colorado",
+                                                   pid="D000197",
+                                                   chamber="house",
+                                                   term_start="2017-01-03",
+                                                   term_end="2019-01-03",
+                                                   party="D",
+                                                   state="CO",
+                                                   cid="N00006134")
+
+            response = self.client.get('/legislators/%d/' % legislator.id)
+            self.assertTemplateUsed(response, 'public-officials/detail.html')
+
+class SenatorPageTest(TestCase):
+    def test_senators_url_resolves_to_senators_index(self):
+        found = resolve('/senators/')
+        self.assertEqual(found.func, senator_index)
+
+    def test_senator_index_page_returns_correct_template(self):
+        Legislator.objects.create(first_name="Diana",
+                                  last_name="Degette",
+                                  phone="12345",
+                                  email="email@email.com",
+                                  state_name="Colorado",
+                                  pid="D000197",
+                                  chamber="senate",
+                                  term_start="2017-01-03",
+                                  term_end="2019-01-03",
+                                  party="D",
+                                  state="CO",
+                                  cid="N00006134")
+        response = self.client.get('/senators/')
+        self.assertTemplateUsed(response, 'public-officials/senators/index.html')
+
+class RepresentativePageTest(TestCase):
+    def test_senators_url_resolves_to_senators_index(self):
+        found = resolve('/representatives/')
+        self.assertEqual(found.func, representative_index)
+
+    def test_senator_index_page_returns_correct_template(self):
+        Legislator.objects.create(first_name="Diana",
+                                  last_name="Degette",
+                                  phone="12345",
+                                  email="email@email.com",
+                                  state_name="Colorado",
+                                  pid="D000197",
+                                  chamber="house",
+                                  term_start="2017-01-03",
+                                  term_end="2019-01-03",
+                                  party="D",
+                                  state="CO",
+                                  cid="N00006134")
+        response = self.client.get('/representatives/')
+        self.assertTemplateUsed(response, 'public-officials/representatives/index.html')
+
+class RecentBillsPageTest(TestCase):
+    def test_recent_bills_url_resolves_to_bills_index_view(self):
+        found = resolve('/recent-bills/')
+        self.assertEqual(found.func, bills_index)
+
+    def test_recent_bills_url_renders_correct_template(self):
+        response = self.client.get('/recent-bills/')
+        self.assertTemplateUsed(response, 'public-officials/bills/index.html')
